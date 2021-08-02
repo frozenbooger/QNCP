a36934ca335e54c9c601ad34e17bdd7375a504fb
frozenbooger <51139053+frozenbooger@users.noreply.github.com>
2021-08-02 12:17:20 -0400

    fixed bug where search would not break if device was unable to identify itself

--- src/QNCP/.ipynb_checkpoints/search-checkpoint.py
+++ src/QNCP/.ipynb_checkpoints/search-checkpoint.py
@@ -12,14 +12,18 @@ def get_resource(inputtype,devicetype,*arg):
         
 def identify(inputtype,devicetype,*arg):
     dev_list = get_resource(inputtype,devicetype,*arg)
-    identity = 0
     id_list = []
     for i in range (0,len(dev_list)):
+        identity = 0
+        count = 0
         while identity == 0:
             try:
                 device = devicetype(dev_list[i],*arg)
                 identity = device.dev.query('*IDN?')
             except:
+                count += 1
                 pass
+            if count == 2: #set the search tolerance i.e this tries to communicate 2 times
+                identity = 'None'
         id_list.append(identity)
     return id_list

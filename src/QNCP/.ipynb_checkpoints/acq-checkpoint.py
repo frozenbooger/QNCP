modified functions to fix bugs with arbitrary waveform generation on DG4000 and scaling on MSO5000

src/QNCP/.ipynb_checkpoints/acq-checkpoint.py
@@ -325,8 +325,22 @@ class Rigol_DMO5000:
         return time_data, volt_data
     
     def scale_offset(self, ch, scale, offset):
-        self.dev.write(':CHANnel{}:SCAL {}'.format(ch,scale))
-        self.dev.write(':CHANnel{}:OFFS {}'.format(ch,offset))
+        """ 
+        Description: The scale_offset function sets the scale and offset parameters of an 
+        oscilliscope channel to the scale of choice 
+
+        Input: ch : channel : integer = {1,2,3,4}
+               scale : usually in the order of your signal/8 : float
+               offset : usually a multiple of the scale : float
+               
+        Output: None : none : None
+        Example: 
+        >> acq1.scale_offset(ch, (max_volt-min_volt)/8, -(max_volt-min_volt)/4)
+
+        np.array([0,1,2,3,4,...,99,100]),np.array([0,1,2,3,2,...,2,3])
+        """
+        self.dev.write(':CHANnel{}:SCAL {}'.format(ch,np.round(scale,4)))
+        self.dev.write(':CHANnel{}:OFFS {}'.format(ch,np.round(offset,4)))
         
     def channel_state(self, ch, state):
         self.dev.write(':CHANnel{}:DISPlay {}'.format(ch,state))

src/QNCP/gen.py
@@ -224,7 +224,7 @@ class Rigol_DG4000:
         
     def arbitrary(self, ch, signal_width, waveform, *arg):
         """
-        Description: Allows one to set and create arbitrary waveform output (Tested 04/03/2022) 
+        Description: Allows one to set and create arbitrary waveform output (Tested 05/27/2022) 
 
         Input: ch : channel : int
                signal_width : width (time) of the argument in seconds : float
@@ -236,11 +236,11 @@ class Rigol_DG4000:
         buffer_size = 2**14
         if inspect.ismethod(waveform) == True or inspect.isfunction(waveform) == True:
             t = np.linspace(0,signal_width,buffer_size)
-            data = np.around(waveform(t,*arg),4)
-            datastring = ",".join(map(str,self.normalize(data)))
+            data = waveform(t,*arg)
+            datastring = ",".join(map(str,np.round(self.normalize(data),4)))
         else:
-            data = np.around(waveform,4)
-            datastring = ",".join(map(str,self.normalize(data)))
+            data = waveform
+            datastring = ",".join(map(str,np.round(self.normalize(data),4)))
         
         factor = max([np.abs(max(data)),np.abs(min(data))])
         self.dev.write('SOURCE{}:Freq {}'.format(ch, 1/signal_width))
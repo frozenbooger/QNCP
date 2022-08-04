1a2a502200505940365cd4061420068077affd59
frozenbooger <51139053+frozenbooger@users.noreply.github.com>
2022-08-03 21:54:58 -0400

    added dll file for qutag driver in package path

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
+import os
 
 setup(
     name='QNCP',
-    version='1.10.2.3',#first: when we get to 10 on second, second: new devices, new package-req., new-functions, three: bugs, four: minor bugs
+    version='1.10.2.4',#first: when we get to 10 on second, second: new devices, new package-req., new-functions, three: bugs, four: minor bugs
     license='MIT',
     author="Leonardo Castillo Veneros and Guodong Cui",
     author_email='frozenbooger@gmail.com',
@@ -26,6 +27,7 @@ setup(
         'python-vxi11'],
     extras_require = {
         'crystal fitting':  ["torch>=1.8"]},
-    package_data={'':['*.dll']}, 
+    package_data={os.path.join('src','qncp'):['*.dll']},
+    data_files = [(os.path.join('lib','site-packages','qncp'),[os.path.join('src','qncp','dependencies','tdcbase_64bit.dll')])],
     zip_safe=False
 )
--- src/QNCP/.ipynb_checkpoints/acq-checkpoint.py
+++ src/QNCP/.ipynb_checkpoints/acq-checkpoint.py
@@ -546,7 +546,7 @@ class qutag:
             raise ValueError('this device is not compatible with your operating system')
         
         package_path = pathlib.Path(pathlib.Path(sys.path[-2]).parents[1])
-        dep_path = pathlib.Path(os.path.join('QNCP', 'dependencies', 'tdcbase_64bit.dll'))
+        dep_path = pathlib.Path(os.path.join('qncp', 'tdcbase_64bit.dll'))
         file_path = str(package_path / dep_path.relative_to(dep_path.anchor))
         # check Python bit version
         if sys.maxsize > 2**32:
